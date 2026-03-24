9824766c22437d9730d06e3682037b9b0382c8d8
Denis <denisdulatov2@gmail.com>
2026-03-25 00:56:47 +0300

    fix leaflet icon typescript error

--- components/MapComponent.tsx
+++ components/MapComponent.tsx
@@ -3,37 +3,24 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import { useEffect } from 'react';
 
-// Создаем кастомные иконки для выделенного и обычного маркеров
-const defaultIcon = L.icon({
-  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
-  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41]
-});
-
-// Красная иконка для выделенного маркера
-const selectedIcon = L.icon({
-  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41]
-});
-
-// Исправляем стандартные иконки
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
-  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
-});
-
+// Исправляем стандартные иконки для Leaflet
+// Делаем это только на клиенте, чтобы избежать ошибок SSR
 export default function MapComponent({ owners, selectedOwner, setSelectedOwner }) {
+  useEffect(() => {
+    // Исправляем иконки только на клиенте
+    if (typeof window !== 'undefined') {
+      // @ts-ignore - игнорируем ошибку TypeScript
+      delete L.Icon.Default.prototype._getIconUrl;
+      L.Icon.Default.mergeOptions({
+        iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
+        iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
+      });
+    }
+  }, []);
+
   const center = selectedOwner 
     ? [selectedOwner.lat, selectedOwner.lng] 
     : [53.195, 45.000];
@@ -52,7 +39,6 @@ export default function MapComponent({ owners, selectedOwner, setSelectedOwner }
         <Marker 
           key={owner.id} 
           position={[owner.lat, owner.lng]}
-          icon={selectedOwner?.id === owner.id ? selectedIcon : defaultIcon}
           eventHandlers={{
             click: () => setSelectedOwner(owner)
           }}
