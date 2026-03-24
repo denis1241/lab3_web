984d89e357ea0cdbb4afd27c556038b52cb5d42a
Denis <denisdulatov2@gmail.com>
2026-03-25 01:06:34 +0300

    last fix

--- components/MapComponent.tsx
+++ components/MapComponent.tsx
@@ -5,6 +5,27 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { useEffect } from 'react';
 
+// Создаем кастомные иконки
+const defaultIcon = L.icon({
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+// Красная иконка для выделенного маркера
+const selectedIcon = L.icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
 // Исправляем стандартные иконки для Leaflet
 export default function MapComponent({ owners, selectedOwner, setSelectedOwner }: any) {
   useEffect(() => {
@@ -39,6 +60,7 @@ export default function MapComponent({ owners, selectedOwner, setSelectedOwner }
         <Marker 
           key={owner.id} 
           position={[owner.lat, owner.lng] as [number, number]}
+          icon={selectedOwner?.id === owner.id ? selectedIcon : defaultIcon}
           eventHandlers={{
             click: () => setSelectedOwner(owner)
           }}
